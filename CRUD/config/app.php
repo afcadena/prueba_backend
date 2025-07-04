e006494892aa1b4dd4e8d6b8ed3b7c103bbdd3e4
ANDRES FELIPE CADENA CASTILLO <AndresF.CadenaC@ugcbogota.edu.co>
2025-07-04 11:15:05 -0500

    Avances proyecto crud

---
+++ CRUD/config/app.php
@@ -0,0 +1,7 @@
+<?php
+
+    const APP_URL="http://localhost/BACKEND/CRUD/";
+    const APP_NAME="CRUD POO MySQL";
+    const APP_SESSION_NAME="CRUD";
+
+    date_default_timezone_set("America/Bogota");
