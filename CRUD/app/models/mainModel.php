2bdfda645b1b99fcd2c58e9c9e82f57dc30e075c
ANDRES FELIPE CADENA CASTILLO <AndresF.CadenaC@ugcbogota.edu.co>
2025-07-04 12:24:54 -0500

    Modelo para eliminar datos de una base de datos mysql

--- CRUD/app/models/mainModel.php
+++ CRUD/app/models/mainModel.php
@@ -111,20 +111,20 @@
         }
 
         protected function actualizarDatos($tabla, $datos,$condicion){  
-        $query ="UPDATE $tabla SET ";
+            $query ="UPDATE $tabla SET ";
         
-        $C=0;
-        foreach($datos as $clave){
+            $C=0;
+            foreach($datos as $clave){
             if($C>=1){$query.=",";}
             $query.=$clave["campo_nombre"]."=".$clave["campo_marcador"];
             $C++;
-        }
+            }
 
-        $query.=" WHERE ".$condicion["condicion_campo"]."=".$condicion["condicion_marcador"];
+            $query.=" WHERE ".$condicion["condicion_campo"]."=".$condicion["condicion_marcador"];
 
-        $sql=$this->conectar()->prepare($query);
+            $sql=$this->conectar()->prepare($query);
 
-        foreach($datos as $clave){
+            foreach($datos as $clave){
                 $sql->bindParam($clave["campo_marcador"],$clave["campo_valor"]);
             }
 
@@ -132,5 +132,14 @@
 
             $sql->execute();
             return $sql;
-         }
+        }
+
+        protected function elimiarRegistro($tabla, $campo,$id){
+            $sql=$this->conectar()->prepare("DELETE FROM $tabla WHERE $campo =:id");
+            $sql->bindParam(":id",$id);
+            $sql->execute();
+            
+            return $sql;
+
+        }
     }
