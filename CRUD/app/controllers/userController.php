1d5f1400d0173565e7e7e2e507eca5d487f2bbc5
ANDRES FELIPE CADENA CASTILLO <AndresF.CadenaC@ugcbogota.edu.co>
2025-07-07 12:19:28 -0500

    Controlador registrar usuarios

--- CRUD/app/controllers/userController.php
+++ CRUD/app/controllers/userController.php
@@ -7,5 +7,71 @@
         #controlador para registrar usuario# 
         public function registrarUsuarioControlador(){
 
+            #almacenando datos#
+            $nombre=$this->limpiarCadena($_POST['usuario_nombre']);
+            $apellido=$this->limpiarCadena($_POST['usuario_apellido']);
+
+            $usuario=$this->limpiarCadena($_POST['usuario_usuario']);
+            $email=$this->limpiarCadena($_POST['usuario_email']);
+            $clave1=$this->limpiarCadena($_POST['usuario_clave_1']);
+            $clave2=$this->limpiarCadena($_POST['usuario_clave_2']);
+
+            #Verificando campos obligatorios#
+            if($nombre =="" || $apellido="" || $usuario="" || $clave1="" || $clave2=""){
+                $alerta=[
+                    "tipo"=>"simple",
+                    "titulo"=>"Ocurrio un error inesperado",
+                    "texto"=>"No has llenado todos los campos que son obligatorios",
+                    "icono"=>"error"
+                ];
+                return json_encode($alerta);
+                exit();
+            }
+
+            # verificando integridad de los datos #
+            if($this->VerificarDatos("[a-zA-ZáéíóúÁÉÍÓÚñÑ ]{3,40}", $nombre)){
+                $alerta=[
+                    "tipo"=>"simple",
+                    "titulo"=>"Ocurrio un error inesperado",
+                    "texto"=>"El nombre no coincide con el formato solicitado",
+                    "icono"=>"error"
+                ];
+                return json_encode($alerta);
+                exit();
+            }
+            if($this->verificarDatos("[a-zA-ZáéíóúÁÉÍÓÚñÑ ]{3,40}",$apellido)){
+		    	$alerta=[
+					"tipo"=>"simple",
+					"titulo"=>"Ocurrió un error inesperado",
+					"texto"=>"El APELLIDO no coincide con el formato solicitado",
+					"icono"=>"error"
+				];
+				return json_encode($alerta);
+		        exit();
+		    }
+
+		    if($this->verificarDatos("[a-zA-Z0-9]{4,20}",$usuario)){
+		    	$alerta=[
+					"tipo"=>"simple",
+					"titulo"=>"Ocurrió un error inesperado",
+					"texto"=>"El USUARIO no coincide con el formato solicitado",
+					"icono"=>"error"
+				];
+				return json_encode($alerta);
+		        exit();
+		    }
+
+		    if($this->verificarDatos("[a-zA-Z0-9$@.-]{7,100}",$clave1) || $this->verificarDatos("[a-zA-Z0-9$@.-]{7,100}",$clave2)){
+		    	$alerta=[
+					"tipo"=>"simple",
+					"titulo"=>"Ocurrió un error inesperado",
+					"texto"=>"Las CLAVES no coinciden con el formato solicitado",
+					"icono"=>"error"
+				];
+				return json_encode($alerta);
+		        exit();
+		    }
+
+
         }
     }
